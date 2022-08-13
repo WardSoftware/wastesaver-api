a04f525755ab36823f670ead267f1dc22dc5a1de
Ryanward02 <ryanward02@gmail.com>
2022-08-13 19:52:58 +0100

    added login and signup page

--- app.py
+++ app.py
@@ -1,7 +1,47 @@
-from flask import Flask
+from flask import Flask, request
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello World!</p>"
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        data = request.get_json()
+        cx = sqlite3.connect("database.db")
+        cu = cx.cursor()
+        dbpwd = cu.execute("select password from users where uname = '" + data['username'].lower() + "'").fetchall()
+        cx.close()
+        if len(dbpwd) == 0:
+            
+            return {
+                'response': "Username Not Found"
+            }
+        else:
+            if data['password'] == dbpwd[0][0]:
+                
+                return {
+                    'response': "Success"
+                }
+            else:
+                return {
+                    'response': "Incorrect Password"
+                }
+        
+    return {
+        'response': "False"
+    }
+
+@app.route("/signup", methods=['POST'])
+def signup():
+    data = request.get_json()
+    cx = sqlite3.connect("database.db")
+    cu = cx.cursor()
+    dbpwd = cu.execute("INSERT INTO Users (uname, password) VALUES (?, ?)", [data['username'].lower(), data['password']])
+    cx.commit()
+
+    cx.close()
+    return {
+        'response': 'Success'
+    }
